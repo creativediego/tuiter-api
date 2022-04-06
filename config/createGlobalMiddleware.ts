d90fc9aa61967365e397adb436705e7ac0f719ed
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-04-06 12:34:45 -0400

    add express session

--- config/createGlobalMiddleware.ts
+++ config/createGlobalMiddleware.ts
@@ -2,6 +2,7 @@ import express, { Express } from 'express';
 import passport from 'passport';
 import cookieSession from 'cookie-session';
 import cors from 'cors';
+import session from 'express-session';
 
 const createGlobalMiddleware = (app: Express) => {
   app.use(express.json());
@@ -13,16 +14,30 @@ const createGlobalMiddleware = (app: Express) => {
       preflightContinue: true,
     })
   );
+  // app.use(
+  //   cookieSession({
+  //     name: 'session',
+  //     keys: [process.env.SESSION_KEYS!],
+  //     maxAge: 24 * 60 * 60 * 100,
+  //     sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'none' : 'lax',
+  //     secure: process.env.NODE_ENV === 'PRODUCTION',
+  //     httpOnly: true,
+  //     domain: process.env.CLIENT_URL!,
+  //   })
+  // );
+
   app.use(
-    cookieSession({
+    session({
       name: 'session',
-      keys: [process.env.SESSION_KEYS!],
-      maxAge: 24 * 60 * 60 * 100,
-      sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'none' : 'lax',
-      secure: process.env.NODE_ENV === 'PRODUCTION',
-      httpOnly: false,
-      overwrite: true,
-      domain: process.env.CLIENT_URL!,
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.SESSION_KEYS!,
+      proxy: true,
+      cookie: {
+        maxAge: 1000 * 60 * 24, // 24 hours
+        secure: true,
+        httpOnly: true,
+      },
     })
   );
 
