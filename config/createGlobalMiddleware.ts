65018db525ac3a6ba74c12b45c13a3bcc01a30d1
Diego <artoriascreative@Diegos-MacBook-Pro.local>
2022-04-06 13:26:53 -0400

    edit session

--- config/createGlobalMiddleware.ts
+++ config/createGlobalMiddleware.ts
@@ -2,7 +2,7 @@ import express, { Express } from 'express';
 import passport from 'passport';
 import cookieSession from 'cookie-session';
 import cors from 'cors';
-import session from 'express-session';
+// import session from 'express-session';
 
 const createGlobalMiddleware = (app: Express) => {
   app.use(express.json());
@@ -14,32 +14,32 @@ const createGlobalMiddleware = (app: Express) => {
       preflightContinue: true,
     })
   );
-  // app.use(
-  //   cookieSession({
-  //     name: 'session',
-  //     keys: [process.env.SESSION_KEYS!],
-  //     maxAge: 24 * 60 * 60 * 100,
-  //     sameSite: process.env.NODE_ENV === 'PRODUCTION' ? 'none' : 'lax',
-  //     secure: process.env.NODE_ENV === 'PRODUCTION',
-  //     httpOnly: true,
-  //     domain: process.env.CLIENT_URL!,
-  //   })
-  // );
-
   app.use(
-    session({
+    cookieSession({
       name: 'session',
-      secret: process.env.SESSION_KEYS!,
-      saveUninitialized: false,
-      resave: false,
-      proxy: true,
-      cookie: {
-        sameSite: process.env.NODE_ENV === 'PRODUCTION' ? false : 'lax',
-        secure: process.env.NODE_ENV === 'PRODUCTION',
-        maxAge: 24 * 60 * 60 * 100,
-      },
+      keys: [process.env.SESSION_KEYS!],
+      maxAge: 24 * 60 * 60 * 100,
+      sameSite: process.env.NODE_ENV === 'PRODUCTION' ? false : 'lax',
+      secure: process.env.NODE_ENV === 'PRODUCTION',
+      httpOnly: true,
+      domain: process.env.CLIENT_URL!,
     })
   );
+
+  // app.use(
+  //   session({
+  //     name: 'session',
+  //     secret: process.env.SESSION_KEYS!,
+  //     saveUninitialized: false,
+  //     resave: false,
+  //     proxy: true,
+  //     cookie: {
+  //       sameSite: process.env.NODE_ENV === 'PRODUCTION' ? false : 'lax',
+  //       secure: process.env.NODE_ENV === 'PRODUCTION',
+  //       maxAge: 24 * 60 * 60 * 100,
+  //     },
+  //   })
+  // );
 
   app.use(passport.initialize());
   app.use(passport.session());
